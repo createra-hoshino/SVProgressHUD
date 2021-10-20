fcc04299b74b1925466a52a26b7482a894f87f0b
Hoshino Hiroyuki <h_hoshino@createra.jp>
2021-10-20 12:06:12 +0900

    Swift Package Manager対応。

---
+++ Tests/SVProgressHUDTests/SVProgressHUDTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SVProgressHUD
+
+final class SVProgressHUDTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SVProgressHUD().text, "Hello, World!")
+    }
+}
